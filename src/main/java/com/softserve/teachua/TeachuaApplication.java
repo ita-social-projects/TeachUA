f5834b500d62339de1f16e12706c84e2b76a992f
Terry <taraskavkalo@gmail.com>
2020-12-31 16:36:53 +0200

    add project configuration

---
+++ src/main/java/com/softserve/teachua/TeachuaApplication.java
@@ -0,0 +1,13 @@
+package com.softserve.teachua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachuaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeachuaApplication.class, args);
+    }
+
+}
