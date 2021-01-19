b7618d13a3e0b22307e88a00f7bbb12363ce2413
mikiyur <mikiyur@gmail.com>
2021-01-19 11:07:25 +0200

    NewsController, RoleController were added

---
+++ src/main/java/com/softserve/teachua/controller/RoleController.java
@@ -0,0 +1,69 @@
+package com.softserve.teachua.controller;
+
+import com.softserve.teachua.dto.RoleResponce;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class RoleController {
+
+    /**
+     * The method which return role.
+     *
+     * @return {@link RoleResponce}
+     * @author Yurii Mykytiuk
+     */
+    @GetMapping("/role/{id}")
+    public RoleResponce getRole(@PathVariable Long id) {
+        // TODO
+        return new RoleResponce();
+    }
+
+    /**
+     * The method which return role.
+     *
+     * @return {@link RoleResponce}
+     * @author Yurii Mykytiuk
+     */
+    @PostMapping("/role/{id}")
+    public RoleResponce addRole() {
+        // TODO
+        return new RoleResponce();
+    }
+
+    /**
+     * The method which return array of existing roles.
+     *
+     * @return {...}
+     * @author Yurii Mykytiuk
+     */
+    @DeleteMapping("/role/{id}")
+    public String deleteRole(@PathVariable Long id) {
+        // TODO
+        return "DeleteMapping, Method deleteRole, role id: " + id;
+    }
+
+    /**
+     * The method which return array of existing roles.
+     *
+     * @return {@link RoleResponce}
+     * @author Yurii Mykytiuk
+     */
+    @GetMapping("/roles")
+    public String getRoles() {
+        // TODO
+        return "GetMapping, Method getRoles";
+    }
+
+    @PostMapping("/roles")
+    public String addRoles() {
+        // TODO
+        return "PostMapping, Method addRoles";
+    }
+
+    @DeleteMapping("/roles")
+    public String deleteRoles() {
+        // TODO
+        return "DeleteMapping, Method deleteRoles";
+    }
+
+}
--- src/main/java/com/softserve/teachua/dto/RoleResponce.java
+++ src/main/java/com/softserve/teachua/dto/RoleResponce.java
@@ -2,11 +2,13 @@ package com.softserve.teachua.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class RoleResponce {
 
-    private String rolename;
-    
+    private String roleName;
+
 }
