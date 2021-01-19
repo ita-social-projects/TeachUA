47ba7c07039a1123c009fa6ef890f6c29fb494ab
bakaydmytro <bakay.dmytro@gmail.com>
2021-01-19 23:48:54 +0200

    Add Category and Center models

--- src/main/java/com/softserve/teachua/model/Center.java
+++ src/main/java/com/softserve/teachua/model/Center.java
@@ -1,35 +1,29 @@
 package com.softserve.teachua.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 @Data
 @Entity
-@Table(name = "users")
-public class User {
+@Table(name = "centers")
 
+public class Center {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
-    @Column
-    private String email;
-    
-    @Column
-    private String password;
 
     @Column
-    private String name;
+    private String city;
 
-    @ManyToOne
-    @JoinColumn(name = "roles_id")
-    private Role role;
+    @Column
+    private String adress;
+
+    @Column
+    private String coordinate;
+
 }
