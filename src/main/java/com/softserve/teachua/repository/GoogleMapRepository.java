2af0d574b82252a55c6dbf8b3fa8f3a326cb2606
mikiyur <mikiyur@gmail.com>
2021-01-22 20:29:50 +0200

    @Param annotation added to GoogleMapRepository

--- src/main/java/com/softserve/teachua/repository/GoogleMapRepository.java
+++ src/main/java/com/softserve/teachua/repository/GoogleMapRepository.java
@@ -4,11 +4,12 @@ import com.softserve.teachua.entity.Coordinates;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GoogleMapRepository extends JpaRepository<Coordinates, Integer> {
 
     @Query(value = "select * from club_coordinates as crd join children_center as cc on cc.coordinates_id=crd.id where cc.address=:centerAddress", nativeQuery = true)
-    Coordinates getClubCoordinatesByAddress(String centerAddress);
+    Coordinates getClubCoordinatesByAddress(@Param("centerAddress") String centerAddress);
 }
