1500547fc744e577ac9d78b8a80b56050448d5f7
bakaydmytro <bakay.dmytro@gmail.com>
2021-01-28 11:51:00 +0200

    update successcreatedcenter class

--- src/main/java/com/softserve/teachua/dto/controller/SuccessCreatedCity.java
+++ src/main/java/com/softserve/teachua/dto/controller/SuccessCreatedCity.java
@@ -1,6 +1,5 @@
 package com.softserve.teachua.dto.controller;
 
-import com.softserve.teachua.model.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,14 +12,4 @@ import lombok.NoArgsConstructor;
 public class SuccessCreatedCity {
     private Long id;
     private String name;
-    private String email;
-    private String address;
-    private String phones;
-    private String description;
-    private String urlWeb;
-    private String urlLogo;
-    private String socialLinks;
-    private Double latitude;
-    private Double longitude;
-    private User user;
 }
