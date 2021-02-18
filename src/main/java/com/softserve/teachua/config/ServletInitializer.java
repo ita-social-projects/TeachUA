35249c151b9dbfeab1141b55cb90eee2b75b5e77
Denis Burko <ealightpawn@gmail.com>
2021-02-18 20:39:11 +0200

    Added config for war packaging

---
+++ src/main/java/com/softserve/teachua/config/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.softserve.teachua.config;
+
+import com.softserve.teachua.Application;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+}
