1773fc8f9d3d403ccd9eebb36330a0725195b2fd
888kozyr <888kozyr@gmail.com>
2021-02-28 11:33:12 +0200

    added Contact Type model

---
+++ src/main/java/com/softserve/teachua/model/ContactType.java
@@ -0,0 +1,28 @@
+package com.softserve.teachua.model;
+
+import com.softserve.teachua.dto.marker.Convertible;
+import com.softserve.teachua.model.marker.Archivable;
+import lombok.*;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@With
+@Builder
+@Entity
+@Table(name = "contact_type")
+public class ContactType implements Convertible, Archivable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String urlLogo;
+
+}
