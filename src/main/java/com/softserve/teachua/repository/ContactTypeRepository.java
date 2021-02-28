a156f37d3708d6f4a0a6c5c232cd7b38d81f0a43
888kozyr <888kozyr@gmail.com>
2021-02-28 11:34:53 +0200

    added repository layer for Contact Type

---
+++ src/main/java/com/softserve/teachua/repository/ContactTypeRepository.java
@@ -0,0 +1,13 @@
+package com.softserve.teachua.repository;
+
+import com.softserve.teachua.model.ContactType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ContactTypeRepository extends JpaRepository<ContactType, Long> {
+
+    boolean existsByName(String name);
+
+    Optional<ContactType> findByName(String name);
+}
