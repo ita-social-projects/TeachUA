1d9a980e20f6bd68200b9b442d77774a7fa1adf4
Denis Burko <ealightpawn@gmail.com>
2021-03-19 12:37:13 +0200

    some changes

--- src/main/java/com/softserve/teachua/config/ApplicationConfig.java
+++ src/main/java/com/softserve/teachua/config/ApplicationConfig.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.servlet.Filter;
 import javax.servlet.RequestDispatcher;
