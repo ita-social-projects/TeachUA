6675b87e3f46a748196acea1964b131cc0b19284
jekaBas <jekisbas@gmail.com>
2021-05-07 20:18:58 +0300

    fix add logs

--- src/main/java/com/softserve/teachua/service/impl/LogServiceImpl.java
+++ src/main/java/com/softserve/teachua/service/impl/LogServiceImpl.java
@@ -1,36 +1,22 @@
 package com.softserve.teachua.service.impl;
 
+import com.softserve.teachua.service.LogService;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.filefilter.TrueFileFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
-public class LogServiceImpl implements com.softserve.teachua.service.LogService {
-    private static final String PATH = "target/logs";
-//    public Map<String, List<String>> getAllLogs() {
-//        Map<String, List<String>> result = new HashMap<>();
-//
-//        List<File> files = (List<File>) FileUtils.listFiles(new File(PATH), TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE);
-//
-//        for (int i = 0; i < files.size(); i++) {
-//            try {
-//                result.put(files.get(i).getName(), FileUtils.readLines(files.get(i), StandardCharsets.UTF_8));
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//
-//        return result;
-//    }
+public class LogServiceImpl implements LogService {
+
+    @Value(value = "${logging.file.path}")
+    private String PATH;
 
     @Override
     public List<String> getAllLogs() {
@@ -47,7 +33,7 @@ public class LogServiceImpl implements com.softserve.teachua.service.LogService 
         try {
             result = FileUtils.readLines(new File(PATH + "/" + name), StandardCharsets.UTF_8);
             for (String s : result) {
-                s = s + "\\n";
+                s += "\\n";
             }
         } catch (IOException e) {
             e.printStackTrace();
