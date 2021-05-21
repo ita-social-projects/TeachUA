b82ebac48e7ec21822a64b302689a0392421026d
Vitalii <kindratvit24@gmail.com>
2021-05-21 14:26:19 +0300

    Add validation for location name and age fields

--- src/main/java/com/softserve/teachua/dto/location/LocationProfile.java
+++ src/main/java/com/softserve/teachua/dto/location/LocationProfile.java
@@ -5,13 +5,23 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Data
 public class LocationProfile {
     private Long id;
+
+    @Pattern(regexp = "^(?!\\s)([\\wА-ЩЬЮЯҐЄІЇа-щьюяґєії !\\\"#$%&'()*+,\\-.\\/:;<=>?@\\]\\[^_`{}~]){5,100}$" ,
+             message = "Це поле може містити тільки українські та англійські літери, цифри та спеціальні символи’")
+    @Pattern(regexp = "^.*\\S$",
+            message = "Це поле може містити тільки українські та англійські літери, цифри та спеціальні символи’")
     private String name;
+
     private String address;
     private String cityName;
     private String districtName;
