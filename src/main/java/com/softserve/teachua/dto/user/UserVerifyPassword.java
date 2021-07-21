919bfe732824f995b9a443bc80121a3c61fb8038
Roman Soluk <roman.soluk25@gmail.com>
2021-07-21 22:40:50 +0300

    add password change and verification

---
+++ src/main/java/com/softserve/teachua/dto/user/UserVerifyPassword.java
@@ -0,0 +1,21 @@
+package com.softserve.teachua.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserVerifyPassword {
+
+    @NotNull
+    private Long id;
+
+    @NotEmpty
+    private String password;
+
+}
