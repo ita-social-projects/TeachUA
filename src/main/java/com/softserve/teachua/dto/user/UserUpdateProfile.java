52d2e403f76f92886cdfa0f2d9552a06be65bde9
Roman Soluk <roman.soluk25@gmail.com>
2021-08-03 11:07:19 +0300

    remove not working annotations

--- src/main/java/com/softserve/teachua/dto/user/UserUpdateProfile.java
+++ src/main/java/com/softserve/teachua/dto/user/UserUpdateProfile.java
@@ -1,14 +1,11 @@
 package com.softserve.teachua.dto.user;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.softserve.teachua.dto.marker.Convertible;
-import javafx.scene.image.Image;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotEmpty;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,22 +15,16 @@ public class UserUpdateProfile implements Convertible {
 
     private Long id;
 
-    //@NotEmpty
     private String email;
 
-    //@NotEmpty
     private String firstName;
 
-    //@NotEmpty
     private String lastName;
 
-    //@NotEmpty
     private String phone;
 
-    //@NotEmpty
     private String password;
 
-    //private byte[] urlLogo;
     private String urlLogo;
 
     private String status;
