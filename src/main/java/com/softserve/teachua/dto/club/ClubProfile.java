f628f5cae308131e47afc0b6b567a4e26b11bf77
OH72 <gemboleg@gmail.com>
2021-10-20 17:03:27 +0300

    Delete validation package

--- src/main/java/com/softserve/teachua/dto/club/ClubProfile.java
+++ src/main/java/com/softserve/teachua/dto/club/ClubProfile.java
@@ -1,7 +1,6 @@
 package com.softserve.teachua.dto.club;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.softserve.teachua.dto.club.validation.ClubDescription;
 import com.softserve.teachua.dto.gallery.GalleryPhotoProfile;
 import com.softserve.teachua.dto.location.LocationProfile;
 import com.softserve.teachua.dto.marker.Convertible;
@@ -22,18 +21,16 @@ import java.util.List;
 
 public class ClubProfile implements Convertible {
 
-    private Long id;
-
     private List<String> categoriesName;
 
     @Valid
     private List<LocationProfile> locations;
 
     @Valid
-    @ClubDescription
     @JsonIgnoreProperties(ignoreUnknown = true)
-//    @Pattern(regexp = "^[А-Яа-яёЁЇїІіЄєҐґa-zA-Z0-9()\\\\!\\\"\\\"#$%&'*\\n+\\r, ,\\-.:;\\\\<=>—«»„”“–’‘?|@_`{}№~^/\\[\\]]{40,1500}$" ,
-//            message = "Це поле може містити тільки українські та англійські літери, цифри та спеціальні символи’")
+    @Pattern(regexp = "^[А-Яа-яёЁЇїІіЄєҐґa-zA-Z0-9()\\\\!\\\"\\\"#$%&'*\\n+\\r, ,\\-.:;\\\\<=>—«»„”“–’‘?|@_`{}№~^/\\[\\]]{40,1500}$" ,
+            message = "Це поле може містити тільки українські та англійські літери, цифри та спеціальні символи. " +
+                    "Розмір поля повинен бути не менше 40 і не більше 1500 символів.’")
     public String description;
 
     private String name;
