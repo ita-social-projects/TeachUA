c79bc4f1ff9ee4885f934ba5d13008ded7fbb850
Roman Duvalo <duvalo.roman@gmail.com>
2021-10-25 20:51:34 +0300

    Updated validation for UserUpdateProfile

--- src/main/java/com/softserve/teachua/dto/user/UserUpdateProfile.java
+++ src/main/java/com/softserve/teachua/dto/user/UserUpdateProfile.java
@@ -6,6 +6,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,17 +19,29 @@ public class UserUpdateProfile implements Convertible {
 
     private Long id;
 
+    @NotBlank
     private String email;
 
+    @NotBlank
+    @Size(min = 1, max = 25)
+    @Pattern(regexp = "[a-zA-Zа-яА-ЯіІєЄїЇґҐ]*$", message = "First name can contain only ukrainian and english letters")
+    @Pattern(regexp = "^[^ЁёЪъЫыЭэ]*$", message = "First name cannot contain russian letters ")
     private String firstName;
 
+    @NotBlank
+    @Size(min = 1, max = 25)
+    @Pattern(regexp = "[a-zA-Zа-яА-ЯіІєЄїЇґҐ]*$", message = "Last name can contain only ukrainian and english letters")
+    @Pattern(regexp = "^[^ЁёЪъЫыЭэ]*$", message = "Last name cannot contain russian letters ")
     private String lastName;
 
+    @NotBlank
+    @Pattern(regexp = "[0-9]{9}", message = "Phone number must contain 9 numbers and can`t contain other symbols")
     private String phone;
 
     private String urlLogo;
 
     private String status;
 
+    @NotBlank
     private String roleName;
 }
