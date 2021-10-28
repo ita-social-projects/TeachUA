179b8e6951043769e0b2a132bddad2d367f23603
Roman Klymus <klimusr@gmail.com>
2021-10-28 11:46:48 +0300

    some regex fixed

--- src/main/java/com/softserve/teachua/dto/task/UpdateTask.java
+++ src/main/java/com/softserve/teachua/dto/task/UpdateTask.java
@@ -20,7 +20,7 @@ import java.time.LocalDate;
 public class UpdateTask implements Convertible {
     @JsonDeserialize(using = TrimDeserialize.class)
     @NotBlank
-    @Pattern(regexp = "^[А-Яа-яЇїІіЄєҐґa-zA-Z0-9()!\"#$%&'*+ ,-.:;<=>?|@_`{}~^&&[^ыЫъЪёЁэЭ]]+$",
+    @Pattern(regexp = "^[А-Яа-яЇїІіЄєҐґa-zA-Z0-9()!\"#$%&'*+ ,-.:;<=>?|@_`{}~\\\\^&&[^ыЫъЪёЁэЭ]]+$",
             message = "can contains only letters of ukrainian and english languages, numbers, and some special symbols like: [\"#$%&'*+ ,-.:;<=>?|@_`{}~]")
     @Size(min = 5, max = 30, message = "must contain a minimum of 5 and a maximum of 30 letters")
     private String name;
