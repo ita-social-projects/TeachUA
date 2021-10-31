ee3afd81fc7ed508484dfa0c4e941a34b6e1163b
OH72 <gemboleg@gmail.com>
2021-10-31 18:40:13 +0200

    update bad request exception

--- src/main/java/com/softserve/teachua/exception/BadRequestException.java
+++ src/main/java/com/softserve/teachua/exception/BadRequestException.java
@@ -1,5 +1,6 @@
 package com.softserve.teachua.exception;
 
+import javassist.tools.web.BadHttpRequest;
 import org.springframework.lang.Nullable;
 
 /**
@@ -11,7 +12,7 @@ import org.springframework.lang.Nullable;
  *
  * @author Denis Burko
  */
-public class BadRequestException extends  {
+public class BadRequestException extends Exception {
     public static final String CANT_FIND_FIELD_IN_JSON = "Can't find %s in JSON body";
     public static final String JSON_IS_NOT_READABLE = "JSON is not readable";
 
@@ -24,5 +25,7 @@ public class BadRequestException extends  {
 
     public BadRequestException() {
         super(BAD_REQUEST_EXCEPTION);
+
+        new BadHttpRequest();
     }
 }
