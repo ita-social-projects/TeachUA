a848ca328fab999603b0d5867257811a3bc5550b
OH72 <gemboleg@gmail.com>
2021-11-29 23:58:20 +0200

    Needless import

--- src/main/java/com/softserve/teachua/controller/ClubController.java
+++ src/main/java/com/softserve/teachua/controller/ClubController.java
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
