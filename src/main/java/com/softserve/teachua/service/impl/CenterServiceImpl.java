b30d1fb70f13259c3c01a7d5c42828193b9bfd4c
OH72 <gemboleg@gmail.com>
2021-12-07 19:50:42 +0200

    Remove logs from info level

--- src/main/java/com/softserve/teachua/service/impl/CenterServiceImpl.java
+++ src/main/java/com/softserve/teachua/service/impl/CenterServiceImpl.java
@@ -145,7 +145,7 @@ public class CenterServiceImpl implements CenterService, ArchiveMark<Center> {
       
         if (clubsId != null && !clubsId.isEmpty()) {
             for (Long id : clubsId) {
-                log.info("ID - " + id);
+                log.debug("ID - " + id);
                 Club club = clubService.getClubById(id);
                 club.setCenter(center);
                 clubRepository.save(club);
@@ -230,7 +230,7 @@ public class CenterServiceImpl implements CenterService, ArchiveMark<Center> {
 
         archiveModel(center);
 
-        log.info("center {} was successfully deleted", center);
+        log.debug("center {} was successfully deleted", center);
         return dtoConverter.convertToDto(center, CenterResponse.class);
     }
 
