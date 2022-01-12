4de28e98a938c4bff8e4712827e9455f6b3712ac
TarasLyzunyk <mr.lyzunyk@gmail.com>
2022-01-12 10:35:25 +0200

    Refactoring code

--- src/main/java/com/softserve/teachua/controller/PdfGenerationController.java
+++ src/main/java/com/softserve/teachua/controller/PdfGenerationController.java
@@ -1,7 +1,9 @@
 package com.softserve.teachua.controller;
 
 import com.softserve.teachua.controller.marker.Api;
+import com.softserve.teachua.dto.club.ClubResponse;
 import com.softserve.teachua.dto.feedback.FeedbackResponse;
+import com.softserve.teachua.service.ClubService;
 import com.softserve.teachua.service.FeedbackService;
 import com.softserve.teachua.service.PdfGenerationService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,17 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PdfGenerationController implements Api {
-    PdfGenerationService<FeedbackResponse> pdfGenerationService;
-    FeedbackService feedbackService;
+    PdfGenerationService<ClubResponse> pdfGenerationService;
+    ClubService clubService;
 
     @Autowired
-    public PdfGenerationController(PdfGenerationService<FeedbackResponse> pdfGenerationService, FeedbackService feedbackService) {
+    public PdfGenerationController(PdfGenerationService<ClubResponse> pdfGenerationService, ClubService clubService) {
         this.pdfGenerationService = pdfGenerationService;
-        this.feedbackService = feedbackService;
+        this.clubService = clubService;
     }
 
-    @GetMapping(value = {"/pdf"}, produces = {"application/pdf"})
+    @GetMapping(value = {"/pdf/club"}, produces = {"application/pdf"})
     public byte[] generateFeedbackPdf(@RequestParam Long id) {
-        return this.pdfGenerationService.getPdfOutput(this.feedbackService.getFeedbackProfileById(id));
+        return this.pdfGenerationService.getPdfOutput(this.clubService.getClubProfileById(id));
     }
 }
--- src/main/java/com/softserve/teachua/service/impl/PdfGenerationServiceImpl.java
+++ src/main/java/com/softserve/teachua/service/impl/PdfGenerationServiceImpl.java
@@ -1,8 +1,11 @@
 package com.softserve.teachua.service.impl;
 
+import com.softserve.teachua.dto.club.ClubResponse;
 import com.softserve.teachua.dto.feedback.FeedbackResponse;
 import com.softserve.teachua.model.Category;
+import com.softserve.teachua.model.Club;
 import com.softserve.teachua.model.Location;
+import com.softserve.teachua.service.ClubService;
 import com.softserve.teachua.service.PdfGenerationService;
 import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JRException;
@@ -14,6 +17,7 @@ import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
@@ -31,7 +35,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
-public class PdfGenerationServiceImpl implements PdfGenerationService<FeedbackResponse> {
+public class PdfGenerationServiceImpl implements PdfGenerationService<ClubResponse> {
     private static final Logger LOGGER = LoggerFactory.getLogger(PdfGenerationServiceImpl.class.getName());
     private static final String TEMPLATE_PATH = "/pdf-reports/template/FeedbackTemplate.jrxml";
     private static final String IMAGE_PATH = "/pdf-reports/image/";
@@ -40,10 +44,17 @@ public class PdfGenerationServiceImpl implements PdfGenerationService<FeedbackRe
     private static final String IMAGE_PATH_KIDS_JUMP = "club/kids_jump.png";
     private static final String IMAGE_PATH_PENCILS = "club/pencils.jpg";
 
-    public byte[] getPdfOutput(final FeedbackResponse feedbackResponse) {
+    protected ClubService clubService;
+
+    @Autowired
+    public PdfGenerationServiceImpl(final ClubService clubService) {
+        this.clubService = clubService;
+    }
+
+    public byte[] getPdfOutput(final ClubResponse clubResponse) {
         final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         try {
-            JasperPrint jasperPrint = createJasperPrint(feedbackResponse);
+            JasperPrint jasperPrint = createJasperPrint(clubResponse);
             JasperExportManager.exportReportToPdfStream(jasperPrint, byteArrayOutputStream);
             return byteArrayOutputStream.toByteArray();
         } catch (IOException | JRException e) {
@@ -52,24 +63,25 @@ public class PdfGenerationServiceImpl implements PdfGenerationService<FeedbackRe
         return new byte[0];
     }
 
-    private JasperPrint createJasperPrint(FeedbackResponse feedbackResponse) throws JRException, IOException {
+    private JasperPrint createJasperPrint(ClubResponse clubResponse) throws JRException, IOException {
         try (InputStream inputStream = new FileInputStream(getRealFilePath(TEMPLATE_PATH))) {
             final JasperReport jasperReport = JasperCompileManager.compileReport(inputStream);
-            return JasperFillManager.fillReport(jasperReport, getParameters(feedbackResponse), getDataSource(feedbackResponse));
+            return JasperFillManager.fillReport(jasperReport, getParameters(clubResponse), getDataSource(clubResponse));
         }
     }
 
-    private Map<String, Object> getParameters(FeedbackResponse feedbackResponse) throws IOException {
+    private Map<String, Object> getParameters(ClubResponse clubResponse) throws IOException {
         Map<String, Object> parameters = new HashMap<>();
+        parameters.put("club", getClub(clubResponse.getName()));
         parameters.put("CONTEXT_IMAGE", getRealFilePath(IMAGE_PATH));
-        parameters.put("logo", getRealFilePath(IMAGE_PATH + getCategoryImageName(feedbackResponse)));
-        parameters.put("categories", getCategories(feedbackResponse));
-        parameters.put("category-color", getCategoryColor(feedbackResponse));
-        parameters.put("location", getLocationAddress(feedbackResponse));
+        parameters.put("logo", getRealFilePath(IMAGE_PATH + getCategoryImageName(clubResponse)));
+        parameters.put("categories", getCategories(clubResponse));
+        parameters.put("category-color", getCategoryColor(clubResponse));
+        parameters.put("location", getLocationAddress(clubResponse));
         parameters.put("locationImage", getLocationImage());
-        parameters.put("years", getYears(feedbackResponse));
-        parameters.put("contacts", getContacts(feedbackResponse));
-        parameters.put("description", getDescription(feedbackResponse));
+        parameters.put("years", getYears(clubResponse));
+        parameters.put("contacts", getContacts(clubResponse));
+        parameters.put("description", getDescription(clubResponse));
         parameters.put("img-balls", getClubImage(IMAGE_PATH_BALLS));
         parameters.put("img-exercise", getClubImage(IMAGE_PATH_EXERCISE));
         parameters.put("img-kids-jump", getClubImage(IMAGE_PATH_KIDS_JUMP));
@@ -81,28 +93,28 @@ public class PdfGenerationServiceImpl implements PdfGenerationService<FeedbackRe
         return getRealFilePath(IMAGE_PATH + imagePath);
     }
 
-    private String getDescription(FeedbackResponse feedbackResponse) {
+    private String getDescription(ClubResponse clubResponse) {
         StringBuilder description = new StringBuilder();
-        description.append(feedbackResponse.getClub().getDescription());
+        description.append(getClub(clubResponse.getName()).getDescription());
         description.delete(0, 379);
         return description.substring(0, description.indexOf("type") - 3);
     }
 
-    private String getContacts(FeedbackResponse feedbackResponse) {
-        return feedbackResponse.getClub().getContacts();
+    private String getContacts(ClubResponse clubResponse) {
+        return getClub(clubResponse.getName()).getContacts();
     }
 
-    private String getYears(FeedbackResponse feedbackResponse) {
-        return "від " + feedbackResponse.getClub().getAgeFrom() +
-                " до " + feedbackResponse.getClub().getAgeTo() + " років";
+    private String getYears(ClubResponse clubResponse) {
+        return "від " + getClub(clubResponse.getName()).getAgeFrom() +
+                " до " + getClub(clubResponse.getName()).getAgeTo() + " років";
     }
 
     private String getLocationImage() throws IOException {
         return getRealFilePath(IMAGE_PATH + "cluster.png");
     }
 
-    private String getLocationAddress(FeedbackResponse feedbackResponse) {
-        Optional<Location> location = feedbackResponse.getClub().getLocations().stream().findFirst();
+    private String getLocationAddress(ClubResponse clubResponse) {
+        Optional<Location> location = getClub(clubResponse.getName()).getLocations().stream().findFirst();
         if (location.isPresent()) {
             return location.get().getAddress();
         } else {
@@ -111,15 +123,15 @@ public class PdfGenerationServiceImpl implements PdfGenerationService<FeedbackRe
         }
     }
 
-    private Set<String> getCategories(FeedbackResponse feedbackResponse) {
-        return feedbackResponse.getClub().getCategories()
+    private Set<String> getCategories(ClubResponse clubResponse) {
+        return getClub(clubResponse.getName()).getCategories()
                 .stream()
                 .map(Category::getName)
                 .collect(Collectors.toSet());
     }
 
-    private String getCategoryColor(FeedbackResponse feedbackResponse) {
-        Optional<Category> category = feedbackResponse.getClub().getCategories().stream().findFirst();
+    private String getCategoryColor(ClubResponse clubResponse) {
+        Optional<Category> category = getClub(clubResponse.getName()).getCategories().stream().findFirst();
         if (category.isPresent()) {
             return category.get().getBackgroundColor();
         }
@@ -127,8 +139,8 @@ public class PdfGenerationServiceImpl implements PdfGenerationService<FeedbackRe
         return "";
     }
 
-    private String getCategoryImageName(FeedbackResponse feedbackResponse) {
-        Optional<Category> category = feedbackResponse.getClub().getCategories().stream().findFirst();
+    private String getCategoryImageName(ClubResponse clubResponse) {
+        Optional<Category> category = getClub(clubResponse.getName()).getCategories().stream().findFirst();
         if (category.isPresent()) {
             String urlLogo = category.get().getUrlLogo();
             return urlLogo.substring(urlLogo.indexOf("categories/") + 11);
@@ -143,7 +155,11 @@ public class PdfGenerationServiceImpl implements PdfGenerationService<FeedbackRe
         return resourcePath.toFile().getAbsolutePath();
     }
 
-    private JRDataSource getDataSource(FeedbackResponse feedbackResponse) {
-        return new JRBeanCollectionDataSource(Collections.singletonList(feedbackResponse));
+    private JRDataSource getDataSource(ClubResponse clubResponse) {
+        return new JRBeanCollectionDataSource(Collections.singletonList(clubResponse));
+    }
+
+    private Club getClub(String name) {
+        return clubService.getClubByName(name);
     }
 }
