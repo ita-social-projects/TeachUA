3c193796aa0e40f0ee3c562fe7fc2253178e008a
OH72 <gemboleg@gmail.com>
2022-01-20 12:13:00 +0200

    UATeachChallenge

--- src/main/java/com/softserve/teachua/tools/service/transfer_impl/TaskTransferServiceImpl.java
+++ src/main/java/com/softserve/teachua/tools/service/transfer_impl/TaskTransferServiceImpl.java
@@ -5,6 +5,7 @@ import com.softserve.teachua.dto.task.SuccessCreatedTask;
 import com.softserve.teachua.dto.task.TaskProfile;
 import com.softserve.teachua.model.Task;
 import com.softserve.teachua.repository.TaskRepository;
+import com.softserve.teachua.service.ChallengeService;
 import com.softserve.teachua.tools.FileUtils;
 import com.softserve.teachua.tools.repository.TaskInfoRepository;
 import com.softserve.teachua.tools.service.TaskTransferService;
@@ -21,19 +22,29 @@ public class TaskTransferServiceImpl implements TaskTransferService {
     private final FileUtils fileUtils;
     private final DtoConverter dtoConverter;
     private final TaskRepository taskRepository;
+    private final ChallengeService challengeService;
+
+    private final String uaTeachChallenge = "Навчай українською челендж"; // id=1
+    private final String languageChallenge = "Мовомаратон"; // id=2
+    private String uaTeachChallengeImage = "data_for_db/task_images/challengeUA.jpg";
+    private String languageChallengeImage = "data_for_db/task_images/marathon_log.png";
+    private static final String targetPackage = "tasks";
+
 
     @Autowired
     public TaskTransferServiceImpl(
             FileUtils fileReader,
             DtoConverter dtoConverter,
-            TaskRepository taskRepository) {
+            TaskRepository taskRepository, ChallengeService challengeService) {
         this.fileUtils = fileReader;
         this.dtoConverter = dtoConverter;
         this.taskRepository = taskRepository;
+        this.challengeService = challengeService;
     }
 
     @Override
     public List<SuccessCreatedTask> createTasksFromFile(String filePath) {
+        moveImages();
         return createTasks(fileUtils.readFromFile(filePath, TaskProfile.class));
     }
 
@@ -42,13 +53,27 @@ public class TaskTransferServiceImpl implements TaskTransferService {
         return createTasks(TaskInfoRepository.getTasksList());
     }
 
+    private void moveImages(){
+        uaTeachChallengeImage = fileUtils.moveImage(uaTeachChallengeImage, targetPackage);
+        languageChallengeImage = fileUtils.moveImage(languageChallengeImage, targetPackage);
+    }
+
     private List<SuccessCreatedTask> createTasks(List<TaskProfile> tasks) {
         return tasks
                 .stream()
                 .map(taskProfile -> {
-                    if(!taskProfile.getPicture().isEmpty()) {
-                        taskProfile.setPicture(fileUtils.moveImage(taskProfile.getPicture(), "tasks"));
+//                    if(!taskProfile.getPicture().isEmpty()) {
+//                        taskProfile.setPicture(fileUtils.moveImage(taskProfile.getPicture(), "tasks"));
+//                    }
+//                    temporary implementation (uses only once)
+                    if(taskProfile.getChallengeId() == 1){
+                        taskProfile.setChallengeId(challengeService.getChallengeByName(uaTeachChallenge).getId());
+                        taskProfile.setPicture(uaTeachChallengeImage);
+                    }else{
+                        taskProfile.setChallengeId(challengeService.getChallengeByName(languageChallengeImage).getId());
+                        taskProfile.setPicture(languageChallengeImage);
                     }
+
                     log.info("add task: " + taskProfile);
                     return dtoConverter.convertToEntity(taskProfile, Task.builder().build()).withId(null);
                 }).map(taskRepository::save)
