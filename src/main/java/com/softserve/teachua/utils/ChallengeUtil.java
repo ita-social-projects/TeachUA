adf2268c3eafc7b1217efa78ba2862bc8e507733
Taras Lyzunyk <mr.lyzunyk@gmail.com>
2022-04-20 14:15:38 +0300

    Add validation for sortnumbers in challenges

---
+++ src/main/java/com/softserve/teachua/utils/ChallengeUtil.java
@@ -0,0 +1,13 @@
+package com.softserve.teachua.utils;
+
+import com.softserve.teachua.exception.IncorrectInputException;
+
+import java.util.List;
+
+public class ChallengeUtil {
+    public static void validateSortNumber(Long sortNumber, List<Long> unavailableSortNumbers) {
+        if (unavailableSortNumbers.contains(sortNumber) || sortNumber <= 0) {
+            throw new IncorrectInputException("Sort number must be unique and greater than 0");
+        }
+    }
+}
