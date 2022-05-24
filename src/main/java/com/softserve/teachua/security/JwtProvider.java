b6b3ea4641ef44865780cd64c1fc302502d74964
bakaydmytro <bakay.dmytro@gmail.com>
2022-05-24 23:05:52 +0300

    Change lifetime of token to 1 hour

--- src/main/java/com/softserve/teachua/security/JwtProvider.java
+++ src/main/java/com/softserve/teachua/security/JwtProvider.java
@@ -4,20 +4,27 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.DateUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 @Component
 @Slf4j
 public class JwtProvider {
-    private static final int TOKEN_LIFE_DAYS = 1;
+    private static final int TOKEN_LIFE_HOURS = 1;
 
     @Value("$(jwt.secret)")
     private String jwtSecret;
@@ -30,14 +37,12 @@ public class JwtProvider {
     public String generateToken(Authentication authentication) {
         UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
 
-        Date date = Date.from(LocalDate.now()
-                .plusDays(TOKEN_LIFE_DAYS)
-                .atStartOfDay(ZoneId.systemDefault())
-                .toInstant());
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.HOUR, TOKEN_LIFE_HOURS);
         return Jwts.builder()
                 .setSubject(userPrincipal.getEmail())
                 .setId(Long.toString(userPrincipal.getId()))
-                .setExpiration(date)
+                .setExpiration(calendar.getTime())
                 .signWith(SignatureAlgorithm.HS512, jwtSecret)
                 .compact();
     }
