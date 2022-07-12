51938b86e3e7ab5d90aaa85af947249c481b5ebe
Nazar Vashchyshyn <nazar.ua87@gmail.com>
2022-07-12 11:30:36 +0300

    fixed regexp in lang validator

--- src/main/java/com/softserve/teachua/config/ApplicationConfig.java
+++ src/main/java/com/softserve/teachua/config/ApplicationConfig.java
@@ -5,9 +5,12 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import javax.servlet.Filter;
 import javax.servlet.RequestDispatcher;
@@ -21,32 +24,31 @@ public class ApplicationConfig {
     private static final String API_LOCATION = "/api/";
     private static final String SLASH = "/";
     private static final String SWAGGER_UI = "/swagger";
-	  private static final String SWAGGER_RESOURCE = "api-docs";
-  
+    private static final String SWAGGER_RESOURCE = "api-docs";
+
     @Value("${server.servlet.context-path}")
     private String rootUri; // ="";
 
     private String removeSecondSlash(String uri) {
         return uri.replace("//", "/").replace(":/", "://");
     }
-  
 
-  @Bean
-	public FilterRegistrationBean customFilterBean() {
-		FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
-		filterFilterRegistrationBean.setFilter((request, response, chain) -> {
-			HttpServletRequest req = (HttpServletRequest) request;
-			if (!req.getRequestURI().startsWith(removeSecondSlash(rootUri + UPLOAD_LOCATION))
-					&& !req.getRequestURI().startsWith(removeSecondSlash(rootUri + STATIC_LOCATION))
-					&& !req.getRequestURI().startsWith(removeSecondSlash(rootUri + API_LOCATION))
-					&& !req.getRequestURI().equals(removeSecondSlash(rootUri + SLASH))
-					&& !req.getRequestURI().startsWith(removeSecondSlash(rootUri + SWAGGER_UI))
-					&& !req.getRequestURI().contains(SWAGGER_RESOURCE)
-			) {
-				RequestDispatcher requestDispatcher = request.getRequestDispatcher(SLASH);
-				requestDispatcher.forward(request, response);
-				return;
-			}
+    @Bean
+    public FilterRegistrationBean customFilterBean() {
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter((request, response, chain) -> {
+            HttpServletRequest req = (HttpServletRequest) request;
+            if (!req.getRequestURI().startsWith(removeSecondSlash(rootUri + UPLOAD_LOCATION))
+                    && !req.getRequestURI().startsWith(removeSecondSlash(rootUri + STATIC_LOCATION))
+                    && !req.getRequestURI().startsWith(removeSecondSlash(rootUri + API_LOCATION))
+                    && !req.getRequestURI().equals(removeSecondSlash(rootUri + SLASH))
+                    && !req.getRequestURI().startsWith(removeSecondSlash(rootUri + SWAGGER_UI))
+                    && !req.getRequestURI().contains(SWAGGER_RESOURCE)
+            ) {
+                RequestDispatcher requestDispatcher = request.getRequestDispatcher(SLASH);
+                requestDispatcher.forward(request, response);
+                return;
+            }
 
             chain.doFilter(request, response);
         });
