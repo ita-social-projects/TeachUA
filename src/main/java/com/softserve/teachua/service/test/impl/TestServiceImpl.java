317e013329be40c9ee18f9d110ee1ebabde540d3
hehetenya <tetyanak0102@gmail.com>
2022-07-28 08:20:32 +0300

    Edited Subscription (added Group field), added methods to TestServiceImpl.

--- src/main/java/com/softserve/teachua/service/test/impl/TestServiceImpl.java
+++ src/main/java/com/softserve/teachua/service/test/impl/TestServiceImpl.java
@@ -1,5 +1,6 @@
 package com.softserve.teachua.service.test.impl;
 
+import com.softserve.teachua.model.test.Test;
 import com.softserve.teachua.repository.test.TestRepository;
 import com.softserve.teachua.service.test.TestService;
 import lombok.RequiredArgsConstructor;
@@ -7,10 +8,24 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Slf4j
 @Transactional
 @Service
 public class TestServiceImpl implements TestService {
     private final TestRepository testRepository;
+
+    public List<Test> findActiveTests(){
+        return testRepository.findActiveTests();
+    }
+
+    public List<Test> findArchivedTests(){
+        return testRepository.findArchivedTests();
+    }
+
+    public List<Test> findUnarchivedTests(){
+        return testRepository.findUnarchivedTests();
+    }
 }
