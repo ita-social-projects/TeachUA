507195d3a92afcc542088e99a48baec1779891c0
Oleksii <shostak.ok@gmail.com>
2022-08-16 09:17:22 +0300

    Update ScheduleSendService

---
+++ src/main/java/com/softserve/teachua/controller/ScheduleSendEmailController.java
@@ -0,0 +1,10 @@
+package com.softserve.teachua.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class ScheduleSendEmailController {
+}
