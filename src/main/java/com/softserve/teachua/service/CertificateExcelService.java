c4be3a0a5cd42f934612fad44549a3051e7539c4
julia <jkhomyn138@gmail.com>
2022-08-19 15:44:38 +0300

    Updated javadoc

--- src/main/java/com/softserve/teachua/service/CertificateExcelService.java
+++ src/main/java/com/softserve/teachua/service/CertificateExcelService.java
@@ -9,8 +9,10 @@ import org.springframework.web.multipart.MultipartFile;
 public interface CertificateExcelService {
 
     /**
-     * The method parses excel
+     * This method parses excel-file and returns {@code ExcelParsingResponse}
+     * of mistakes and created dto
      *
+     * @param multipartFile - put bode of excel-file to parse
      * @return new {@code ExcelParsingResponse}.
      */
     ExcelParsingResponse parseExcel(MultipartFile multipartFile);
