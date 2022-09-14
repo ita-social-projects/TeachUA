5b5a4ccaa6509c4b5fd67eae23b6cf2d01119df5
julia <jkhomyn138@gmail.com>
2022-09-14 11:24:49 +0300

    Updated excel-file validation

---
+++ src/main/java/com/softserve/teachua/utils/validations/CertificateUserName.java
@@ -0,0 +1,24 @@
+package com.softserve.teachua.utils.validations;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Use this annotation to check userName field on valid length,
+ * and valid words.
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = CertificateUserNameValidator.class)
+public @interface CertificateUserName {
+
+    String message() default "Неможливо розпізнати ім'я та прізвище";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
