4d091d326eea9df4f415c58896803581a9d6fae6
Jaroslav <garasym@gmail.com>
2022-09-30 23:30:48 +0300

    The certificate template has been updated

--- src/main/java/com/softserve/teachua/utils/CertificateContentDecorator.java
+++ src/main/java/com/softserve/teachua/utils/CertificateContentDecorator.java
@@ -14,7 +14,7 @@ import java.util.Locale;
 public class CertificateContentDecorator {
     public final static String PATTERN_WITHOUT_MONTH_AND_YEAR = "з d";
     public final static String PATTERN_WITHOUT_YEAR = "з d MMMM";
-    public final static String PATTERN_WITH_MONTH_AND_YEAR = " до d MMMM yyyy року";
+    public final static String PATTERN_WITH_MONTH_AND_YEAR = " по d MMMM yyyy року";
 
     public String formHours(Integer hours) {
         return "Тривалість навчання - " + hours + " годин.";
