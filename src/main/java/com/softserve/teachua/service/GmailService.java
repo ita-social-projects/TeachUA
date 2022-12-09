19eb6e691a151da0d104ce6a7b3caef14815c79d
Denys Vekeryk <den.vekeryk@gmail.com>
2022-12-09 02:44:44 +0200

    Refactored structure

--- src/main/java/com/softserve/teachua/service/GmailService.java
+++ src/main/java/com/softserve/teachua/service/GmailService.java
@@ -1,26 +1,13 @@
 package com.softserve.teachua.service;
 
-import com.google.api.services.gmail.model.Message;
-import com.softserve.teachua.model.Certificate;
-
-import java.util.List;
-
 /**
  * This service contains methods to manage Gmail API
  */
 public interface GmailService {
 
     /**
-     * This method returns certificates from database according to sendToEmail and sendDate of message with certificate
-     *
-     * @param messageWithCertificate first message of mailer-daemon thread (contains certificate)
-     * @return list os failed sent certificates
-     */
-    List<Certificate> getCertificatesByMessage(Message messageWithCertificate);
-
-    /**
-     * This method checks inbox for unread thread with mailer-daemon@googlemail.com,
-     * gets first message and mark its certificate as unsent
+     * This method checks gmail inbox for unread threads with mailer-daemon@googlemail.com,
+     * then gets first message and mark its certificate as unsent
      */
     void detectFailedSendCertificates();
 }
--- src/main/java/com/softserve/teachua/service/impl/GmailServiceImpl.java
+++ src/main/java/com/softserve/teachua/service/impl/GmailServiceImpl.java
@@ -35,7 +35,26 @@ public class GmailServiceImpl implements GmailService {
     }
 
     @Override
-    public List<Certificate> getCertificatesByMessage(Message messageWithCertificate) {
+    public void detectFailedSendCertificates() {
+        try {
+            ListThreadsResponse threadsResponse = getMessagesResponse(1L);
+            for (Thread thread : threadsResponse.getThreads()) {
+                Thread fullThread = getThreadById(thread.getId());
+                List<Certificate> certificates = getCertificatesByMessage(fullThread.getMessages().get(0));
+                if (!certificates.isEmpty()) {
+                    certificates.forEach(certificate ->
+                        certificateService.updateDateAndSendStatus(certificate.getId(), false));
+                    removeUnreadLabelFromThread(thread.getId());
+                    log.info("Updated {} failed certificates for {}",
+                        certificates.size(), certificates.get(0).getSendToEmail());
+                }
+            }
+        } catch (IOException e) {
+            log.error("IOException while process gmail inbox {}", e.getMessage());
+        }
+    }
+
+    private List<Certificate> getCertificatesByMessage(Message messageWithCertificate) {
         List<MessagePartHeader> headers = messageWithCertificate.getPayload().getHeaders();
         MessagePartHeader dateHeader = headers.get(2);
         MessagePartHeader sentToHeader = headers.get(4);
@@ -55,26 +74,6 @@ public class GmailServiceImpl implements GmailService {
                 userEmail, sendDate);
         }
         return certificates;
-    }
-
-    @Override
-    public void detectFailedSendCertificates() {
-        try {
-            ListThreadsResponse threadsResponse = getMessagesResponse(1L);
-            for (Thread thread : threadsResponse.getThreads()) {
-                Thread fullThread = getThreadById(thread.getId());
-                List<Certificate> certificates = getCertificatesByMessage(fullThread.getMessages().get(0));
-                if (!certificates.isEmpty()) {
-                    certificates.forEach(certificate ->
-                        certificateService.updateDateAndSendStatus(certificate.getId(), false));
-                    removeUnreadLabelFromThread(thread.getId());
-                    log.info("Updated {} failed certificates for {}",
-                        certificates.size(), certificates.get(0).getSendToEmail());
-                }
-            }
-        } catch (IOException e) {
-            log.error("IOException while process gmail inbox {}", e.getMessage());
-        }
     }
 
     private ListThreadsResponse getMessagesResponse(long maxResults) throws IOException {
