4287d8c0db6edfbf4a21994e438c3c7daf35211b
DanBug <unwantedmouth@gmail.com>
2023-01-04 21:40:43 +0200

    Init commit

---
+++ src/test/java/com/softserve/teachua/service/test/CertificateDatesServiceImplTest.java
@@ -0,0 +1,43 @@
+package com.softserve.teachua.service.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CertificateDatesServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void archiveModel() {
+    }
+
+    @Test
+    void restoreModel() {
+    }
+
+    @Test
+    void getCertificateDatesById() {
+    }
+
+    @Test
+    void addCertificateDates() {
+    }
+
+    @Test
+    void getCertificateDatesByDate() {
+    }
+
+    @Test
+    void getCertificateDatesByHoursAndDate() {
+    }
+
+    @Test
+    void getCertificateDatesByDuration() {
+    }
+
+    @Test
+    void getCertificateDatesByDurationAndDate() {
+    }
+}
