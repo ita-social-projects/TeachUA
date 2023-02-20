82c2960e47064e2c3235a169a97ca00f0e6a09d5
Denys Vekeryk <den.vekeryk@gmail.com>
2023-02-20 13:25:39 +0200

    Tested AllowedRolesAspect

--- src/main/java/com/softserve/teachua/security/CustomUserDetailsService.java
+++ src/main/java/com/softserve/teachua/security/CustomUserDetailsService.java
@@ -3,20 +3,18 @@ package com.softserve.teachua.security;
 import com.softserve.teachua.model.User;
 import com.softserve.teachua.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
     public static final String SECURITY_CONTEXT_ERROR =
-            "Attempt to get authenticated user failed. Check Authentication";
+            "There was attempt to get UserPrincipal, but Security Context is empty.";
     private final UserRepository userRepository;
 
     public CustomUserDetailsService(UserRepository userRepository) {
@@ -36,7 +34,7 @@ public class CustomUserDetailsService implements UserDetailsService {
             return ((UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         } catch (Exception e) {
             log.error(SECURITY_CONTEXT_ERROR, e);
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, SECURITY_CONTEXT_ERROR);
+            return null;
         }
     }
 }
---
+++ src/test/java/com/softserve/teachua/utils/annotation/AllowedRolesAspectTest.java
@@ -0,0 +1,70 @@
+package com.softserve.teachua.utils.annotation;
+
+import static com.softserve.teachua.TestUtils.getUser;
+import static com.softserve.teachua.TestUtils.getUserPrincipal;
+import com.softserve.teachua.constants.RoleData;
+import com.softserve.teachua.exception.UserAuthenticationException;
+import com.softserve.teachua.exception.UserPermissionException;
+import com.softserve.teachua.model.User;
+import com.softserve.teachua.security.CustomUserDetailsService;
+import com.softserve.teachua.security.UserPrincipal;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.reflect.MethodSignature;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class AllowedRolesAspectTest {
+    @Mock
+    private CustomUserDetailsService customUserDetailsService;
+    @Mock
+    private ProceedingJoinPoint jp;
+    @Mock
+    private MethodSignature methodSignature;
+    private AllowedRolesAspect allowedRolesAspect;
+    private User user;
+
+    @BeforeEach
+    void setUp() throws NoSuchMethodException {
+        allowedRolesAspect = new AllowedRolesAspect(customUserDetailsService);
+        when(jp.getSignature()).thenReturn(methodSignature);
+        when(methodSignature.getMethod()).thenReturn(this.getClass().getMethod("methodForAdmin"));
+        user = getUser();
+    }
+
+    @Test
+    void givenUserPrincipalNull_thenThrowUserAuthenticationException() {
+        when(customUserDetailsService.getUserPrincipal()).thenReturn(null);
+        assertThrows(UserAuthenticationException.class, () -> allowedRolesAspect.doSomething(jp));
+    }
+
+    @Test
+    void givenRoleAdmin_thenProceed() throws Throwable {
+        user.getRole().setName(RoleData.ADMIN.getDBRoleName());
+        UserPrincipal userPrincipal = getUserPrincipal(user);
+        when(customUserDetailsService.getUserPrincipal()).thenReturn(userPrincipal);
+
+        allowedRolesAspect.doSomething(jp);
+        verify(jp).proceed();
+    }
+
+    @Test
+    void givenRoleUser_thenThrowUserPermissionException() {
+        user.getRole().setName(RoleData.USER.getDBRoleName());
+        UserPrincipal userPrincipal = getUserPrincipal(user);
+        when(customUserDetailsService.getUserPrincipal()).thenReturn(userPrincipal);
+
+        assertThrows(UserPermissionException.class, () -> allowedRolesAspect.doSomething(jp));
+    }
+
+    @AllowedRoles(RoleData.ADMIN)
+    public void methodForAdmin() {
+        // Dummy method to be used in the tests
+    }
+}
