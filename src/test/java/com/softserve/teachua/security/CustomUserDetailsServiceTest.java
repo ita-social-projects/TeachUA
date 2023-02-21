405105f16026fa3b54a8aae2e7e1fa9bb054ac6d
Denys Vekeryk <den.vekeryk@gmail.com>
2023-02-21 23:10:23 +0200

    Covered getUserPrincipal() with tests

--- src/test/java/com/softserve/teachua/security/CustomUserDetailsServiceTest.java
+++ src/test/java/com/softserve/teachua/security/CustomUserDetailsServiceTest.java
@@ -3,6 +3,7 @@ package com.softserve.teachua.security;
 import static com.softserve.teachua.TestConstants.USER_EMAIL;
 import static com.softserve.teachua.TestUtils.getUser;
 import static com.softserve.teachua.TestUtils.getUserPrincipal;
+import com.softserve.teachua.exception.UserAuthenticationException;
 import com.softserve.teachua.model.User;
 import com.softserve.teachua.repository.UserRepository;
 import java.util.Optional;
@@ -16,6 +17,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
@@ -54,5 +57,26 @@ class CustomUserDetailsServiceTest {
 
         assertThatThrownBy(() -> userDetailsService.loadUserByUsername(USER_EMAIL))
                 .isInstanceOf(UsernameNotFoundException.class);
+    }
+
+    @Test
+    void shouldReturnUserPrincipalFromSecurityContext_whenGetUserPrincipal() {
+        SecurityContextHolder.getContext()
+                .setAuthentication(new UsernamePasswordAuthenticationToken(userDetails, null));
+
+        UserPrincipal actual = userDetailsService.getUserPrincipal();
+        assertAll("Should return appropriate UserDetails",
+                () -> assertEquals(userDetails.getUsername(), actual.getUsername()),
+                () -> assertEquals(userDetails.getPassword(), actual.getPassword()),
+                () -> assertEquals(userDetails.getAuthorities(), actual.getAuthorities())
+        );
+    }
+
+    @Test
+    void shouldThrowUserAuthenticationException_whenGetUserPrincipal() {
+        SecurityContextHolder.getContext().setAuthentication(null);
+
+        assertThatThrownBy(() -> userDetailsService.getUserPrincipal())
+                .isInstanceOf(UserAuthenticationException.class);
     }
 }
