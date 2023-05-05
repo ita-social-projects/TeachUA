64ca36063f1defea5a721d73c5fab8883d857c82
BNazar002 <bilyk.nazar00@gmail.com>
2023-05-05 17:34:55 +0300

    Enabled spring method security

--- src/main/java/com/softserve/teachua/config/SecurityConfig.java
+++ src/main/java/com/softserve/teachua/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -26,6 +27,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 @Order(1)
+@EnableMethodSecurity
 public class SecurityConfig {
     private static final String ADMIN = RoleData.ADMIN.getRoleName();
     private static final String USER = RoleData.USER.getRoleName();
