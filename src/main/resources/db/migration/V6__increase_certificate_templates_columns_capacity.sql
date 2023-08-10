b356e5a81dc15e7cf85b59dda4aaa779386dbbd4
i.mikhalov <112470475+larbadge@users.noreply.github.com>
2023-08-10 11:23:28 +0300

    Revert formatting changes

--- src/main/resources/db/migration/V6__increase_certificate_templates_columns_capacity.sql
+++ src/main/resources/db/migration/V6__increase_certificate_templates_columns_capacity.sql
@@ -1,4 +1,2 @@
-alter table certificate_templates
-    alter column project_description type character varying(1023);
-alter table certificate_templates
-    alter column course_description type character varying(1023);
+alter table certificate_templates alter column project_description type character varying(1023);
+alter table certificate_templates alter column course_description type character varying(1023);
