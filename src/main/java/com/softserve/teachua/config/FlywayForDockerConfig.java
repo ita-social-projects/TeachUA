8cecb792760668d910924f81b93c7c4bc9f81058
i.mikhalov <mihalovivan90@gmail.com>
2023-08-28 14:22:17 +0300

    wrote config for flyway starting after hibernate

---
+++ src/main/java/com/softserve/teachua/config/FlywayForDockerConfig.java
@@ -0,0 +1,29 @@
+package com.softserve.teachua.config;
+
+import lombok.RequiredArgsConstructor;
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import javax.sql.DataSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class FlywayForDockerConfig {
+    private final DataSource dataSource;
+    @Value("${flyway.for.docker.enabled}")
+    private boolean isEnabled;
+
+    @EventListener(ContextRefreshedEvent.class)
+    public void migrateFlywayAfterHibernate() {
+        if (isEnabled) {
+            Flyway flyway = Flyway.configure()
+                    .dataSource(dataSource)
+                    .locations("classpath:/db/docker")
+                    .baselineOnMigrate(true)
+                    .load();
+            flyway.migrate();
+        }
+    }
+}
