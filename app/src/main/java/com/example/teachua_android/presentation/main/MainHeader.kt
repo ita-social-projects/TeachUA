7bd83d4fd15bfaa1701b13d4f59b8faf7a896893
misha <mrmishj@gmail.com>
2023-09-12 17:01:43 +0300

    Main header bg added + small changes

--- app/src/main/java/com/example/teachua_android/presentation/main/MainHeader.kt
+++ app/src/main/java/com/example/teachua_android/presentation/main/MainHeader.kt
@@ -1,5 +1,6 @@
 package com.example.teachua_android.presentation.main
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -8,10 +9,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
@@ -22,77 +23,96 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.teachua_android.R
 import com.example.teachua_android.presentation.ui.theme.OrangePrimary
 
 
+//TODO(Add font family open sans)
 @Preview(showBackground = true)
 @Composable
-fun MainHeader(){
-    //TODO add location handling
-    val city = "Київ"
-    Column(
-        modifier = Modifier
-            //TODO add bg picture
-            .background(Color.Gray)
-            .fillMaxWidth()
-            .padding(12.dp)
-            .wrapContentHeight()
+fun MainHeader(location: String = "Київ"){
+    val image: Painter = painterResource(id = R.drawable.header_main_bg)
+    Box(
+        modifier = Modifier.heightIn(max = 225.dp),
+        contentAlignment = Alignment.Center
     ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Icon(
-                imageVector = Icons.Default.Menu,
-                contentDescription = "Menu",
-                tint = Color.White
-            )
-            Spacer(modifier = Modifier.width(80.dp))
-            LocationFragment(city)
-            Button(
-                onClick = {
-                /*TODO*/
-                },
-                colors = ButtonDefaults.outlinedButtonColors(containerColor = OrangePrimary),
-                shape = RoundedCornerShape(10.dp),
-            ) {
-                Text(text = "Додати гурток", color = Color.White)
-            }
-        }
-        Row(
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
             modifier = Modifier
-                .padding(vertical = 20.dp)
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
         ) {
-            Text(
-                text = "Гуртки у місті $city",
-                color = Color.White,
-                fontSize = 30.sp
-            )
-            Icon(
-                imageVector = Icons.Default.LocationOn,
-                contentDescription = "Location",
-                tint = Color.White
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(
+                    onClick = { /*TODO(Implement functionallity to burger menu)*/ },
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Menu,
+                        contentDescription = "Menu",
+                        tint = Color.White
+                    )
+                }
+                Spacer(modifier = Modifier.width(80.dp))
+                LocationFragment(location)
+                Button(
+                    onClick = {
+                        /*TODO("Add functionality to add club button")*/
+                    },
+                    colors = ButtonDefaults.outlinedButtonColors(containerColor = OrangePrimary),
+                    shape = RoundedCornerShape(10.dp),
+                ) {
+                    Text(text = "Додати гурток", color = Color.White)
+                }
+            }
+            Row(
+                modifier = Modifier
+                    .padding(vertical = 20.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Гуртки у місті $location",
+                    color = Color.White,
+                    fontSize = 30.sp
+                )
+                Icon(
+                    imageVector = Icons.Default.LocationOn,
+                    contentDescription = "Location",
+                    tint = Color.White
+                )
+            }
+            UnderlinedTextInput(
+                value = "Який гурток шукаєте?",
+                onValueChange = { TODO("Add onValueChange to TextInput to search clubs") }
             )
         }
-        UnderlinedTextInput(
-            value = "Який гурток шукаєте?",
-            onValueChange = { TODO() }
-        )
     }
+
 }
 
 @Composable
@@ -156,7 +176,8 @@ fun LocationFragment(location: String) {
             text = location,
             color = Color.White,
             fontSize = 16.sp,
-            fontWeight = FontWeight.Light
+            //fontFamily = FontFamily(),
+            fontWeight = FontWeight(400),
         )
         Icon(
             imageVector = Icons.Default.ArrowDropDown,
