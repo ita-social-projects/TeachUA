a67020d1e79a888a00ba56b56aeb9bc5b996c359
Taras Yatsynych <yatsynych.t@gmail.com>
2023-11-30 21:18:13 +0100

    Change constant naming convention to uppercase snake_case in searchQueries.constants.js

--- src/test/playwright/constants/searchQueries.constants.js
+++ src/test/playwright/constants/searchQueries.constants.js
@@ -1,10 +1,6 @@
-export const simpleSearchParameters = {
-    CATEGORY_SEARCH_QUERY: "Танці",
-    CLUBNAME_SEARCH_QUERY: "Школа",
-    DESCRIPTION_SEARCH_QUERY: "підлітків в Україні",
-    INCORRECT_SEARCH_QUERY: "not existing"
-}
-
-export const advancedSearchParameters = {
-    
+export const SIMPLE_SEARCH_PARAMETERS = {
+    categorySearchQuery: "Танці",
+    clubNameSearchQuery: "Школа",
+    descriptionSearchQuery: "підлітків в Україні",
+    incorrectSearchQuery: "not existing"
 }
--- src/test/playwright/tests/clubsPage-test.spec.js
+++ src/test/playwright/tests/clubsPage-test.spec.js
@@ -2,7 +2,7 @@ import { test} from "@playwright/test";
 import ApiService from "../services/apiService";
 import ClubsPage from "../PO/ClubsPage";
 import BasePage from "../PO/BasePage";
-import {simpleSearchParameters} from "../constants/searchQueries.constants";
+import {SIMPLE_SEARCH_PARAMETERS} from "../constants/searchQueries.constants";
 import {CITIES} from "../constants/general.constants";
 
 let apiservice, clubspage, basepage;
@@ -20,24 +20,24 @@ test.describe.configure({
 
     test("Verify that clubs received in the result of the simple search contain search query (search by club name/description)", async ({ page }) => {
         //search by the club name and verification
-        await clubspage.simpleSearchByQuery(simpleSearchParameters.CLUBNAME_SEARCH_QUERY);
-        await clubspage.verifyClubCardsContainText(simpleSearchParameters.CLUBNAME_SEARCH_QUERY);
+        await clubspage.simpleSearchByQuery(SIMPLE_SEARCH_PARAMETERS.clubNameSearchQuery);
+        await clubspage.verifyClubCardsContainText(SIMPLE_SEARCH_PARAMETERS.clubNameSearchQuery);
 
         //change the search (by the club description) and verify that the results are correct
-        await clubspage.simpleSearchByQuery(simpleSearchParameters.DESCRIPTION_SEARCH_QUERY);
-        await clubspage.verifyClubCardsContainText(simpleSearchParameters.DESCRIPTION_SEARCH_QUERY);
+        await clubspage.simpleSearchByQuery(SIMPLE_SEARCH_PARAMETERS.descriptionSearchQuery);
+        await clubspage.verifyClubCardsContainText(SIMPLE_SEARCH_PARAMETERS.descriptionSearchQuery);
     });
 
     test("Verify that clubs received in the result of the simple search contain search query (search by category name)", async ({ page }) => {
         //search by the club category and verification/different city
         basepage = new BasePage(page);
         await basepage.selectCityInNavBar(CITIES.kharkiv)
-        await clubspage.simpleSearchByQuery(simpleSearchParameters.CATEGORY_SEARCH_QUERY);
-        await clubspage.verifyClubCardsContainText(simpleSearchParameters.CATEGORY_SEARCH_QUERY);
+        await clubspage.simpleSearchByQuery(SIMPLE_SEARCH_PARAMETERS.categorySearchQuery);
+        await clubspage.verifyClubCardsContainText(SIMPLE_SEARCH_PARAMETERS.categorySearchQuery);
     });
 
     test("Verify that respective message will be shown when there are no results", async ({ page }) => {
-        await clubspage.simpleSearchByQuery(simpleSearchParameters.INCORRECT_SEARCH_QUERY);
+        await clubspage.simpleSearchByQuery(SIMPLE_SEARCH_PARAMETERS.incorrectSearchQuery);
         await clubspage.verifyElementVisibility(clubspage.noResultsMessage);
     });
 
