cb8b55de96027034612258be028d7cc2645d0e3f
Jaroslav <garasym@gmail.com>
2023-12-01 00:17:34 +0200

    Comments updated

--- src/main/java/com/softserve/teachua/service/impl/CertificateByTemplateServiceImpl.java
+++ src/main/java/com/softserve/teachua/service/impl/CertificateByTemplateServiceImpl.java
@@ -35,9 +35,9 @@ import static com.softserve.teachua.service.impl.PdfTemplateServiceImpl.CERTIFIC
 @Component
 public class CertificateByTemplateServiceImpl implements CertificateByTemplateService {
     private static final String HALVAR_BLK_PATH = "certificates/fonts/Halvar Breitschrift Medium.ttf";
-            /*"./src/main/resources/certificates/fonts/Halvar Breitschrift Medium.ttf";*/
+    /*"./src/main/resources/certificates/fonts/Halvar Breitschrift Medium.ttf";*/
     private static final String HALVAR_MD_PATH = "certificates/fonts/Halvar Breitschrift Medium.ttf";
-            /*"./src/main/resources/certificates/fonts/Halvar Breitschrift Medium.ttf";*/
+    /*"./src/main/resources/certificates/fonts/Halvar Breitschrift Medium.ttf";*/
 
     private final QRCodeService qrCodeService;
     private final ObjectMapper objectMapper;
